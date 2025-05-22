c58cca90edc5bfa08a645868fd65273715f3ba02
FAIZANSHABIR12 <151016890+FAIZANSHABIR12@users.noreply.github.com>
2025-05-22 14:34:39 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+if(n%2==0){
+    //even
+    cout<< "n is odd";
+}
+else
+{
+    //odd
+    
+    cout<<"n is even";
+}x
+}
